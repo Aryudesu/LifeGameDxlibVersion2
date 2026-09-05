312e6d70ccb16277e4874cbb992ab9cd29219072
Aryu <suwako_a-u-@hotmail.co.jp>
2026-09-05 21:11:34 +0900

    スクロールとズームに対応した描画を実装

--- LifeGameDxlibVersion2/LifeRenderer.cpp
+++ LifeGameDxlibVersion2/LifeRenderer.cpp
@@ -11,14 +11,30 @@ LifeRenderer::LifeRenderer(int screenWidth, int screenHeight)
       aliveColor_(GetColor(0, 255, 0)) {
 }
 
-void LifeRenderer::draw(const LifeBoard& board) const {
-    const int drawWidth = std::min(screenWidth_, board.width());
-    const int drawHeight = std::min(screenHeight_, board.height());
+void LifeRenderer::draw(const LifeBoard& board, int cameraX, int cameraY, int cellSize) const {
+    const int visibleColumns = (screenWidth_ + cellSize - 1) / cellSize;
+    const int visibleRows = (screenHeight_ + cellSize - 1) / cellSize;
+    const int endX = std::min(board.width(), cameraX + visibleColumns);
+    const int endY = std::min(board.height(), cameraY + visibleRows);
 
-    for (int y = 0; y < drawHeight; ++y) {
-        for (int x = 0; x < drawWidth; ++x) {
-            if (board.isAlive(x, y)) {
-                DrawPixel(x, y, aliveColor_);
+    for (int y = cameraY; y < endY; ++y) {
+        const int screenY = (y - cameraY) * cellSize;
+        for (int x = cameraX; x < endX; ++x) {
+            if (!board.isAlive(x, y)) {
+                continue;
+            }
+
+            const int screenX = (x - cameraX) * cellSize;
+            if (cellSize == 1) {
+                DrawPixel(screenX, screenY, aliveColor_);
+            } else {
+                DrawBox(
+                    screenX,
+                    screenY,
+                    screenX + cellSize - 1,
+                    screenY + cellSize - 1,
+                    aliveColor_,
+                    TRUE);
             }
         }
     }
