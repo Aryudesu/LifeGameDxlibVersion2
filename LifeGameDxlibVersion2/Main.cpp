c8c5c6a7e238c0d15f0493ab96f2ab656211e620
Aryu <suwako_a-u-@hotmail.co.jp>
2026-09-05 21:11:49 +0900

    カメラのスクロールとズームを追加

--- LifeGameDxlibVersion2/Main.cpp
+++ LifeGameDxlibVersion2/Main.cpp
@@ -2,31 +2,69 @@
 #include "LifeBoard.h"
 #include "LifeRenderer.h"
 
+#include <algorithm>
+
 namespace {
 constexpr int ScreenWidth = 1024;
 constexpr int ScreenHeight = 1024;
 constexpr int BoardWidth = 1536;
 constexpr int BoardHeight = 1536;
+constexpr int MinCellSize = 1;
+constexpr int MaxCellSize = 32;
+constexpr int CameraMoveSpeed = 8;
 
-void editBoardWithMouse(LifeBoard& board) {
+void clampCamera(const LifeBoard& board, int cellSize, int& cameraX, int& cameraY) {
+    const int visibleColumns = (ScreenWidth + cellSize - 1) / cellSize;
+    const int visibleRows = (ScreenHeight + cellSize - 1) / cellSize;
+    const int maxCameraX = std::max(0, board.width() - visibleColumns);
+    const int maxCameraY = std::max(0, board.height() - visibleRows);
+
+    cameraX = std::clamp(cameraX, 0, maxCameraX);
+    cameraY = std::clamp(cameraY, 0, maxCameraY);
+}
+
+void updateCamera(const LifeBoard& board, int cellSize, int& cameraX, int& cameraY) {
+    if (CheckHitKey(KEY_INPUT_LEFT) != 0) {
+        cameraX -= CameraMoveSpeed;
+    }
+    if (CheckHitKey(KEY_INPUT_RIGHT) != 0) {
+        cameraX += CameraMoveSpeed;
+    }
+    if (CheckHitKey(KEY_INPUT_UP) != 0) {
+        cameraY -= CameraMoveSpeed;
+    }
+    if (CheckHitKey(KEY_INPUT_DOWN) != 0) {
+        cameraY += CameraMoveSpeed;
+    }
+
+    clampCamera(board, cellSize, cameraX, cameraY);
+}
+
+void editBoardWithMouse(LifeBoard& board, int cameraX, int cameraY, int cellSize) {
     int mouseX = 0;
     int mouseY = 0;
     GetMousePoint(&mouseX, &mouseY);
 
     if (mouseX < 0 || mouseY < 0 ||
-        mouseX >= ScreenWidth || mouseY >= ScreenHeight ||
-        mouseX >= board.width() || mouseY >= board.height()) {
+        mouseX >= ScreenWidth || mouseY >= ScreenHeight) {
+        return;
+    }
+
+    const int boardX = cameraX + mouseX / cellSize;
+    const int boardY = cameraY + mouseY / cellSize;
+    if (boardX < 0 || boardY < 0 ||
+        boardX >= board.width() || boardY >= board.height()) {
         return;
     }
 
     const int mouseInput = GetMouseInput();
 
     if ((mouseInput & MOUSE_INPUT_LEFT) != 0) {
-        board.setAlive(mouseX, mouseY, true);
+        board.setAlive(boardX, boardY, true);
     }
 
     if ((mouseInput & MOUSE_INPUT_RIGHT) != 0) {
-        board.setAlive(mouseX, mouseY, false);
+        board.setAlive(boardX, boardY, false);
     }
 }
 }
@@ -51,6 +89,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
     bool paused = false;
     bool enterWasDown = false;
     bool spaceWasDown = false;
+    bool plusWasDown = false;
+    bool minusWasDown = false;
+    int cameraX = 0;
+    int cameraY = 0;
+    int cellSize = 1;
 
     while (ProcessMessage() == 0) {
         const bool enterIsDown = CheckHitKey(KEY_INPUT_RETURN) != 0;
@@ -66,8 +109,24 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
         const bool singleStepRequested = paused && spaceIsDown && !spaceWasDown;
         spaceWasDown = spaceIsDown;
 
+        const bool plusIsDown = CheckHitKey(KEY_INPUT_ADD) != 0;
+        if (plusIsDown && !plusWasDown && cellSize < MaxCellSize) {
+            cellSize *= 2;
+            clampCamera(board, cellSize, cameraX, cameraY);
+        }
+        plusWasDown = plusIsDown;
+
+        const bool minusIsDown = CheckHitKey(KEY_INPUT_SUBTRACT) != 0;
+        if (minusIsDown && !minusWasDown && cellSize > MinCellSize) {
+            cellSize /= 2;
+            clampCamera(board, cellSize, cameraX, cameraY);
+        }
+        minusWasDown = minusIsDown;
+
+        updateCamera(board, cellSize, cameraX, cameraY);
+
         if (paused) {
-            editBoardWithMouse(board);
+            editBoardWithMouse(board, cameraX, cameraY, cellSize);
             if (singleStepRequested) {
                 board.step();
             }
@@ -76,7 +135,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
         }
 
         ClearDrawScreen();
-        renderer.draw(board);
+        renderer.draw(board, cameraX, cameraY, cellSize);
         ScreenFlip();
     }
 
