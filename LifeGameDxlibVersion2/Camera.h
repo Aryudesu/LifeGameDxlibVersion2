bba0173a113762bd459284b766c044e52e2199a1
Aryu <suwako_a-u-@hotmail.co.jp>
2026-09-05 22:40:52 +0900

    カーソル基準ズームとドラッグ移動を追加

--- LifeGameDxlibVersion2/Camera.h
+++ LifeGameDxlibVersion2/Camera.h
@@ -14,8 +14,13 @@ public:
     int visibleRows() const noexcept;
 
     void move(int deltaX, int deltaY) noexcept;
+    void panByPixels(int deltaScreenX, int deltaScreenY) noexcept;
+    void endPan() noexcept;
+
     bool zoomIn(int maxCellSize) noexcept;
     bool zoomOut(int minCellSize) noexcept;
+    bool zoomInAt(int screenX, int screenY, int maxCellSize) noexcept;
+    bool zoomOutAt(int screenX, int screenY, int minCellSize) noexcept;
 
     int boardXAtColumn(int column) const noexcept;
     int boardYAtRow(int row) const noexcept;
@@ -29,6 +34,9 @@ private:
     int x_;
     int y_;
     int cellSize_;
+    int panRemainderX_ = 0;
+    int panRemainderY_ = 0;
 
     static int wrap(int value, int size) noexcept;
+    bool zoomAt(int screenX, int screenY, int newCellSize) noexcept;
 };
