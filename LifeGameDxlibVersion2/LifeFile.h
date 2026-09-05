8f0139ff0393671f87d80de38aa07960f4208fe9
Aryu <suwako_a-u-@hotmail.co.jp>
2026-09-05 22:59:21 +0900

    セーブデータに世代数を渡せるようにする

--- LifeGameDxlibVersion2/LifeFile.h
+++ LifeGameDxlibVersion2/LifeFile.h
@@ -1,10 +1,19 @@
 #pragma once
 
+#include <cstdint>
 #include <string>
 
 class LifeBoard;
 
 namespace LifeFile {
-bool save(const LifeBoard& board, const std::string& path, std::string& errorMessage);
-bool load(LifeBoard& board, const std::string& path, std::string& errorMessage);
+bool save(
+    const LifeBoard& board,
+    std::uint64_t generation,
+    const std::string& path,
+    std::string& errorMessage);
+bool load(
+    LifeBoard& board,
+    std::uint64_t& generation,
+    const std::string& path,
+    std::string& errorMessage);
 }
