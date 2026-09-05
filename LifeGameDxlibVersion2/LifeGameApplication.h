f61c1a46c151cd6f14dadb1e3e631e0c8baefbaf
Aryu <suwako_a-u-@hotmail.co.jp>
2026-09-05 22:50:01 +0900

    世代数と速度状態をApplicationに追加

--- LifeGameDxlibVersion2/LifeGameApplication.h
+++ LifeGameDxlibVersion2/LifeGameApplication.h
@@ -5,6 +5,9 @@
 #include "LifeBoard.h"
 #include "LifeRenderer.h"
 
+#include <cstddef>
+#include <cstdint>
+
 class LifeGameApplication {
 public:
     LifeGameApplication();
@@ -12,12 +15,15 @@ public:
     int run();
 
 private:
-    void update(const InputFrame& input);
+    void update(const InputFrame& input, double elapsedSeconds);
     void draw() const;
     void setPaused(bool paused);
     void saveBoard();
     void loadBoard();
     void editBoardWithMouse(const InputFrame& input);
+    void advanceGeneration();
+    void changeSimulationSpeed(int direction);
+    int currentSimulationSpeed() const noexcept;
 
     LifeBoard board_;
     LifeRenderer renderer_;
@@ -25,4 +31,8 @@ private:
     InputController inputController_;
     bool paused_ = false;
     bool showGrid_ = false;
+    std::uint64_t generation_ = 0;
+    std::size_t simulationSpeedIndex_ = 0;
+    double simulationAccumulator_ = 0.0;
+    double fps_ = 0.0;
 };
